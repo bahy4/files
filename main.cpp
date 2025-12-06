b676603ab22fe79a6ffcc413906f565bfcc7d69e
Andrey <andrey-bakhvalov@mail.ru>
2025-12-06 00:17:07 +0000

    File output added

--- main.cpp
+++ main.cpp
@@ -4,16 +4,22 @@
 
 int main() {
     std::ifstream inputFile("input.txt");
+    std::ofstream outputFile("output.txt");
     std::string line;
     
-    if (inputFile.is_open()) {
-        while (std::getline(inputFile, line)) {
-            std::cout << line << std::endl;
-        }
-        inputFile.close();
-    } else {
+    if (!inputFile.is_open()) {
         std::cerr << "Не удалось открыть файл input.txt" << std::endl;
         return 1;
+    }
+    else if(!outputFile.is_open()){
+        std::cerr << "Не удалось открыть файл output.txt" << std::endl;
+        return 1;
+    }
+    else{
+        while (std::getline(inputFile, line)) {
+            outputFile << line << std::endl;
+        }
+        inputFile.close();
     }
     
     return 0;
